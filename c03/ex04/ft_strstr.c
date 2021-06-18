dba836b138f1599c4609ce5b2d3c2bdf6f22d660
dipelaez <dipelaez@student.42sp.org.br>
2021-06-18 14:55:51 +0000

    c02,c03,c04

---
+++ c03/ex04/ft_strstr.c
@@ -0,0 +1,26 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	len;
+	int	i;
+	int	j;
+
+	len = 0;
+	while (to_find[len])
+		len++;
+	if (len == 0)
+		return (str);
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		while (to_find[j] == str[i + j])
+		{
+			if (j + 1 == len)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
